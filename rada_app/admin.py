b8d87ccb153fc83022eae9d1a920191e7c74e554
davex932 <tagnefonodavid@gmail.com>
2025-12-01 16:45:19 +0100

    mise a jour de la BD

--- rada_app/admin.py
+++ rada_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import POST,COMMENT,LIKE
+from .models import POST,COMMENT,LIKE,User
 
 # Register your models here.
 admin.site.register(POST)
 admin.site.register(COMMENT)
 admin.site.register(LIKE)
+admin.site.register(USer)
